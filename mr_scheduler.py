360848ccd1c126d33c7bfba1f797875b2d0c92ae
lakho01 <lakhosh40@gmail.com>
2022-01-30 01:25:26 +0000

    Removed unnecessary comments

--- mr_scheduler.py
+++ mr_scheduler.py
@@ -5,37 +5,38 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
-# @client.event
-# async def servers(ctx):
-#   servers = list(client.guilds)
-#   await ctx.send(f"Connected on {str(len(servers))} servers:")
-#   await ctx.send('\n'.join(guild.name for guild in guilds))
-
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Bot is running as {0.user}'.format(client))
+
+# This is the section where you put in your weekly assignments
+# An example is :
+# Week2 = ("Mathematics Sheet 12 due Friday")
 
 Week0 = ("")
 Week1 = ("")
-Week2 = ("COMP11120 Maths Sheet 11 (Monday)")
-Week3 = ("COMP11120 Maths Sheet 12 (Monday)")
-Week4 = ("COMP11120 Maths Sheet 13 (Monday)")
+Week2 = ("<Redacted>")
+Week3 = ("<Redacted>")
+Week4 = ("<Redacted>")
 Week5 = (
-    "COMP11120 Maths Sheet 14 (Monday) \n COMP11212 Computation Exercise 1 (Friday) \n COMP16412 Programming 2 Coursework - Chess (Friday)")
+    "<Redacted>")
 Week6 = (
-    "COMP11120 Maths Sheet 15 (Monday) \n COMP11212 Computation Exercise 2 (Friday)")
+    "<Redacted>")
 Week7 = (
-    "COMP11120 Maths Sheet 16 (Monday) \n COMP 15212 Operating System Lab3 (Friday)")
-Week8 = ("COMP11120 Maths Sheet 17 (Monday)")
-WeekAB = ("COMP13212 Data Science Lab4 (Friday)")
-Week9 = ("COMP11120 Maths Sheet 18 (Monday)")
+    "<Redacted>")
+Week8 = ("<Redacted>")
+WeekAB = ("<Redacted>")
+Week9 = ("<Redacted>")
 Week10 = (
-    "COMP11120 Maths Sheet 19 (Monday) \n COMP11212 Computation Exercise 3 (Friday) \n COMP13212 Data Science Lab5 (Friday)")
-Week11 = ("COMP11120 Maths Sheet 20 (Monday) \n COMP15212 Operating System Lab5 (Friday) \n COMP16412 Progrmming 2 Coursework - Fungi (Friday) \n COMP11212 Computation Exercise 4 (Friday) \n COMP10120 Team Project Presentation (Friday)")
-Week12 = ("COMP10120 Team Project Final Report (Friday)")
+    "<Redacted>")
+Week11 = ("<Redacted>")
+Week12 = ("<Redacted>")
 
-owner = "<@467264405097283594>"
+# End of section
+
+# For example ("<@8932308402480230>") - include the < and >
+owner = "<Enter your discord user id here>"
 err_msg = "Send a DM to: " + str(owner)
 
 
@@ -48,7 +49,7 @@ async def on_message(message):
             title="Mr Scheduler Help", description="To find out tasks in a week, type $$wk followed by a week number, for example -> $$wk2 ", color=0x00ff00)
         await message.channel.send(embed=embedVar)
 
-    if message.content.startswith("$wk"):
+    if message.content.startswith("$wk"):  # Change of prefix
         embedVar = discord.Embed(
             title="Prefix has been changed", description="Instead of $wk, it is now $$wk ", color=0x00ff00)
         await message.channel.send(embed=embedVar)
@@ -174,23 +175,10 @@ async def on_message(message):
         embedVar = discord.Embed(
             title="That's all! Goodluck with your exams!", color=0x00ff00)
         embedVar.set_image(
-            url="https://media4.giphy.com/media/ZhK8fC14hUxmo/giphy.gif?cid=ecf05e47draeno3afu9jru7nhvjrc5pi88vfkt5cbcua85y4&rid=giphy.gif&ct=g")
-        # embedVar.add_field(name="Tasks:", value=Week12, inline=False)
+            url="https://media4.giphy.com/media/ZhK8fC14hUxmo/giphy.gif?cid=ecf05e47draeno3afu9jru7nhvjrc5pi88vfkt5cbcua85y4&rid=giphy.gif&ct=g")  # Gif taken from GIPHY that wishes you luck
         await message.channel.send(embed=embedVar)
 
-    # if message.content.startswith("$hello"):
-    #     await message.channel.send('Hello!')
-
-    # if message.content == ('$wk49'):
-    #     embedVar = discord.Embed(
-    #         title="Deadlines for Week 11", description="Week 11 runs from 9 May - 13 May", color=0x00ff00)
-    #     embedVar.add_field(name="Tasks:", value=Week11, inline=False)
-    #     embedVar.add_field(name="Do you see any incorrect data?",
-    #                        value=err_msg, inline=False)
-    #     # embedVar.add_field(value=owner, inline=False)
-    #     await message.channel.send(embed=embedVar)
-# token = os.environ.get('Token')
-keep_alive()
-# client.run(token)
+keep_alive()  # Calls the keep_alive() function from keep_running.py
+# Method to get the token that is encrypted in this case
 my_secret = os.environ['Token2']
 client.run(my_secret)
