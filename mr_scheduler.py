a619539a8f32c74099a1c2c854baaf39ef5fe3cc
Ali Lakho <74470867+lakho01@users.noreply.github.com>
2023-10-13 16:38:36 +0100

    importing mr_scheduler 2.0 code
    removing previous version of the bot

--- mr_scheduler.py
+++ mr_scheduler.py
@@ -1,183 +1,210 @@
+from email.mime import image
+from turtle import back, title
+from discord.ext import commands
 import discord
-import os
-
-from keep_alive import keep_alive
-
-client = discord.Client()
-
-
-@client.event
-async def on_ready():
-    print('Bot is running as {0.user}'.format(client))
-
-# This is the section where you put in your weekly assignments
-# An example is :
-# Week2 = ("Mathematics Sheet 12 due Friday")
-
-Week0 = ("")
-Week1 = ("")
-Week2 = ("<Redacted>")
-Week3 = ("<Redacted>")
-Week4 = ("<Redacted>")
-Week5 = (
-    "<Redacted>")
-Week6 = (
-    "<Redacted>")
-Week7 = (
-    "<Redacted>")
-Week8 = ("<Redacted>")
-WeekAB = ("<Redacted>")
-Week9 = ("<Redacted>")
-Week10 = (
-    "<Redacted>")
-Week11 = ("<Redacted>")
-Week12 = ("<Redacted>")
-
-# End of section
+from discord import app_commands
+import interactions
+from datetime import date
+from discord.ui import Button, View
 
 
-owner = "<Enter your discord user id here>" # For example ("<@8932308402480230>") - include the < and >
-err_msg = "Send a DM to: " + str(owner)
+bot = interactions.Client(
+    token=""
+)
+
+#----------------------------Start of CWs------------------------------------------ 
+
+Week1S = ("COMP23111 SWE1 Basic Git IndCwk1")
+Week1F = ("")
+
+Week2S = ("COMP21111 L&M Propositional Logic")
+Week2F = ("")
+
+Week3S = ("COMP21111 L&M Satisfiability \nCOMP23111 SWE1 Conflicts in Git IndCwk2")
+Week3F = ("")
+
+Week4S = ("COMP21111 L&M Propositional Formalisations \nCOMP26020 PLP Lab1 Matrix Multiplication in C \nCOMP22111 PM Lab1 ALU \nCOMP23111 DS Database Design")
+Week4F = ("")
+
+Week5S = ("COMP21111 L&M DPLL/Random Formulas \nCOMP26120 A&DS Recursive Complexity \nCOMP23311 SWE1 Fixing Bugs TeamCwk1")
+Week5F = ("")
+
+Week6S = ("COMP24011 AI Games")
+Week6F = ("")
+
+Week7S = ("COMP21111 L&M Randomized Algorithms/Tableaux \nCOMP26020 PLP Lab2 Library Management in C++")
+Week7F = ("")
+
+Week8S = ("COMP21111 L&M BDT/BDDs/OBDDs")
+Week8F = ("")
+
+Week9S = ("COMP21111 L&M QBF Basics \nCOMP26120 A&DS Spell Checking Data Structures \nCOMP22111 PM Lab3 FSM")
+Week9F = ("")
+
+Week10S = ("COMP21111 L&M QBF Algorithms \nCOMP23111 DS Advanced Databases \nCOMP23311 SWE1 Adding Features TeamCwk2")
+Week10F = ("")
+
+Week11S = ("COMP21111 L&M PLDF / State Transition Systems \nCOMP26020 PLP Haskell \nCOMP24011 AI SLAM")
+Week11F = ("")
+
+Week12S = ("COMP21111 L&M LTL \nCOMP22111 PM Control Implementation \nCOMP22111 PM Signal Using Charts")
+Week12F = ("")
+#----------------------------------------End of stupid section------------------------------------------
+course_abb = "SWE1: Software Engineering 1 \nL&M: Logic & Modelling \nPLP: Programming Languages & Paradigms \nA&DS: Algorithms & Data Structures \nPM: Processor Microarchitecture \nDS: Database Systems \nAI: Introduction to AI"
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == ("_help"):
-        embedVar = discord.Embed(
-            title="Mr Scheduler Help", description="To find out tasks in a week, type $$wk followed by a week number, for example -> $$wk2 ", color=0x00ff00)
-        await message.channel.send(embed=embedVar)
+Dates = {
+    1 : "26 September - 30 September",
+    2 : "03 October - 07 October",
+    3 : "10 October - 14 October",
+    4 : "17 October - 21 October",
+    5 : "24 October - 28 October",
+    6 : "31 October - 4 November",
+    7 : "07 November - 11 November",
+    8 : "14 November - 18 November",
+    9 : "21 November - 25 November",
+    10 : "28 November - 02 December",
+    11 : "05 December - 09 December",
+    12 : "12 December - 16 December"
+}
 
-    if message.content.startswith("$wk"):  # Change of prefix
-        embedVar = discord.Embed(
-            title="Prefix has been changed", description="Instead of $wk, it is now $$wk ", color=0x00ff00)
-        await message.channel.send(embed=embedVar)
+Summatives = {
+    1 : "No tasks",
+    2 : "COMP21111 L&M Propositional Logic \nCOMP23111 SWE1 Basic Git IndCwk1",
+    3 : "COMP21111 L&M Satisfiability",
+    4 : "COMP21111 L&M Propositional Formalisations \nCOMP23111 SWE1 Conflicts in Git IndCwk2 \nCOMP26020 PLP Lab1 Matrix Multiplication in C \nCOMP22111 PM Lab1 ALU \nCOMP23111 DS Database Design",
+    5 : "COMP21111 L&M DPLL/Random Formulas \nCOMP26120 A&DS Recursive Complexity \nCOMP23311 SWE1 Fixing Bugs TeamCwk1",
+    6 : "COMP24011 AI Games",
+    7 : "COMP21111 L&M Randomized Algorithms/Tableaux",
+    8 : "COMP21111 L&M BDT/BDDs/OBDDs",
+    9 : "COMP21111 L&M QBF Basics \nCOMP22111 PM Lab3 FSM",
+    10 : "COMP21111 L&M QBF Algorithms \nCOMP23111 DS Implementation \nCOMP24011 AI Fuzzy Logic \nCOMP26020 PLP Lab2 C++",
+    11 : "COMP21111 L&M PLDF / State Transition Systems \nCOMP26120 A&DS Spell Checking Data Structures \nCOMP23311 SWE1 Adding Features TeamCwk2",
+    12 : "COMP21111 L&M LTL \nCOMP22111 PM Control Implementation \nCOMP22111 PM Signal Using Charts \nCOMP24011 AI SLAM"
+}
 
-    if message.content == ('$$wk0') or message.content == ('$$wk00'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 0", description="Week 0 runs from 31st Jan to 4th Feb", color=0x00ff00)
-        embedVar.add_field(
-            name="Tasks:", value="Currently no tasks for Week 0", inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+Formatives = {
+    1 : "No tasks",
+    2 : "COMP26120 A&DS Analysing Algorithms",
+    3 : "COMP26120 A&DS Introducing Complexity Analysis",
+    4 : "COMP26120 A&DS Spellchecking 1",
+    5 : "No tasks",
+    6 : "No tasks",
+    7 : "No tasks",
+    8 : "COMP26120 A&DS Spellchecking 2",
+    9 : "No tasks",
+    10 : "No tasks",
+    11 : "No tasks",
+    12 : "COMP26120 A&DS Priority Queues"
+}
 
-    if message.content == ('$$wk1') or message.content == ('$$wk01'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 1", description="Week 1 runs from 7 Feb to 11 Feb", color=0x00ff00)
-        embedVar.add_field(
-            name="Tasks:", value="Currently no tasks for Week 1", inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
 
-    if message.content == ('$$wk2') or message.content == ('$$wk02'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 2", description="Week 2 runs from 14 Feb to 18 Feb", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week2, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+#----------------------------End of CWs--------------------------------------
 
-    if message.content == ('$$wk3') or message.content == ('$$wk03'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 3", description="Week 3 runs from 21 Feb to 25 Feb", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week3, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+my_id = "<@467264405097283594>"
+dan_id = "<@455501686023389186>"
+hsdg_id = "<@325213623091986434>"
+help_desc = "Note: The bot is currently being developed, any suggestions please DM: "
+comb_ali_dan = my_id +"\n" + dan_id
+data_link = "https://docs.google.com/spreadsheets/d/11XvSfBHq-kSYZquztFrkjaJJpn6OxDu1uPAkBn-Cie0/edit#gid=1202261217"
 
-    if message.content == ('$$wk4') or message.content == ('$$wk04'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 4", description="Week 4 runs from 28 Feb to 4 March", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week4, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+data_page = interactions.Button(
+    style=interactions.ButtonStyle.LINK,
+    label="Data From",
+    url = data_link,
+    disabled = False
+)
 
-    if message.content == ('$$wk5') or message.content == ('$$wk05'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 5", description="Week 5 runs from 7 March to 11 March", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week5, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+@bot.command(
+    name = "help",
+    description = "View help"
+)
 
-    if message.content == ('$$wk6') or message.content == ('$$wk06'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 6", description="Week 6 runs from 14 March to 18 March", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week6, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+async def help(ctx: interactions.CommandContext):
+    embeds_help = interactions.Embed(
+        title="Mr. Scheduler v2.0",
+        description=help_desc+str(my_id)+" or "+str(dan_id),
+        color=0x00ff00
+        )
+    embeds_help.add_field(name="Developed by", value=comb_ali_dan, inline=False)
+    embeds_help.add_field(name="Tested by", value=hsdg_id, inline=False)
+    embeds_help.add_field(name="Course Abbreviations", value=course_abb, inline=False)
+    await ctx.send(embeds = embeds_help, components = data_page)
 
-    if message.content == ('$$wk7') or message.content == ('$$wk07'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 7", description="Week 7 runs from 21 March to 25 March", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week7, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+global WEEK_NUM
 
-    if message.content == ('$$wk8') or message.content == ('$$wk08'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 8", description="Week 8 runs from 28 March to 1 April", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week8, inline=False)
-        embedVar.add_field(
-            name="Upcoming April Break", value="There is one task due next week, type $wkab to find out", inline=False)
-        embedVar.add_field(
-            name="Caution:", value="Might get april fooled by someone", inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
 
-    if message.content == ('$$wkab'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week - April Break", description="Week - April Break runs from 4 April to 7 April", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=WeekAB, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+@bot.command(
+    name="week_choose",
+    description="Choose a week number",
+    options = [
+        interactions.Option(
+            name="week_number",
+            description="Choose a week number from 1 - 12",
+            type=interactions.OptionType.INTEGER,
+            required=True,
+        ),
+    ],
+)
+async def week_choose(ctx: interactions.CommandContext, week_number: int):
+    # global msg
+    if (week_number>12) or (week_number<1):
+        await ctx.send(":x: Choose between 1 - 12")
+    else:
 
-    if message.content == ('$$wk9') or message.content == ('$$wk09'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 9", description="Week 9 runs from 25 April - 29 April", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week9, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
+        global WEEK_NUM
+        WEEK_NUM = week_number
+        embeds = interactions.Embed(
+        title=f"Deadlines for Week {week_number}",
+        description=Dates[week_number],
+        color=0x00ff00
+        )
+        embeds.add_field(name="Summatives:", value=Summatives[week_number], inline=False)
+        embeds.add_field(name="Formatives:", value=Formatives[week_number], inline=False)
+        if (week_number == 6):
+            embeds.title = (embeds.title+"\nReading Week")
+            embeds.set_thumbnail(url = "https://i.pinimg.com/originals/66/8a/8c/668a8cccacc792924fa588b4adca8f68.gif")
+        await ctx.send(embeds = embeds)
 
-    if message.content == ('$$wk10'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 10", description="Week 10 runs from 2 May - 6 May", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week10, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
 
-    if message.content == ('$$wk11'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 11", description="Week 11 runs from 9 May - 13 May", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week11, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
 
-    if message.content == ('$$wk12'):
-        embedVar = discord.Embed(
-            title="Deadlines for Week 12", description="Week 12 runs from 16 May - 20 May", color=0x00ff00)
-        embedVar.add_field(name="Tasks:", value=Week12, inline=False)
-        embedVar.add_field(name="Do you see any incorrect data?",
-                           value=err_msg, inline=False)
-        await message.channel.send(embed=embedVar)
-    if message.content == ('$$wk13'):
-        embedVar = discord.Embed(
-            title="That's all! Goodluck with your exams!", color=0x00ff00)
-        embedVar.set_image(
-            url="https://media4.giphy.com/media/ZhK8fC14hUxmo/giphy.gif?cid=ecf05e47draeno3afu9jru7nhvjrc5pi88vfkt5cbcua85y4&rid=giphy.gif&ct=g")  # Gif taken from GIPHY that wishes you luck
-        await message.channel.send(embed=embedVar)
 
-keep_alive()  # Calls the keep_alive() function from keep_running.py
-my_secret = os.environ['Token2'] # Method to get the token that is encrypted in this case
-client.run(my_secret)
+    @bot.component("next")
+    async def button_response(ctx):
+        global WEEK_NUM
+        forward = WEEK_NUM
+        WEEK_NUM += 1
+        forward = forward + 1
+        embeds1 = interactions.Embed(
+        title=f"Deadlines for Week {forward}",
+        description=Dates[forward],
+        color=0x00ff00
+        )
+        embeds1.add_field(name="Summatives:", value=Summatives[forward], inline=False)
+        if (forward == 6):
+            embeds1.title = (embeds1.title+"\nReading Week")
+            embeds1.set_thumbnail(url = "https://i.pinimg.com/originals/66/8a/8c/668a8cccacc792924fa588b4adca8f68.gif")
+        await ctx.send(embeds = embeds1, ephemeral=False)
+
+    
+
+    @bot.component("back")
+    async def button_response(ctx):
+        # await msg.delete()
+        backward = WEEK_NUM
+        backward = backward - 1
+        embeds0 = interactions.Embed(
+        title=f"Deadlines for Week {backward}",
+        description=Dates[backward],
+        color=0x00ff00
+        )
+        embeds0.add_field(name="Summatives:", value=Summatives[backward], inline=False)
+        if (backward == 6):
+            embeds0.title = (embeds0.title+"\nReading Week")
+            embeds0.set_thumbnail(url = "https://i.pinimg.com/originals/66/8a/8c/668a8cccacc792924fa588b4adca8f68.gif")
+        await ctx.send(embeds = embeds0, ephemeral=False)
+
+
+bot.start()
+
+#---------------------------------------------End of code----------------------------------------------------
